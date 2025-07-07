cff90f2948517734681858a22a0a40572dd0dddc
charliecooke01 <charlielcooke@googlemail.com>
2025-07-07 23:15:27 +0100

    Add edit button and taks name validation

--- Assessments/Week_04/To_Do_List/assets/js/script.js
+++ Assessments/Week_04/To_Do_List/assets/js/script.js
@@ -10,11 +10,24 @@ createButton.addEventListener("click", onClickCreateTask);
 
 function onClickCreateTask() {
   // Assign task to value
-  var taskValue = createTask.value;
+  var taskValue = createTask.value.trim();
   
+  // check for empty value
+  if (taskValue === "") {
+    alert("Give the task a name");
+    return;
+  }
+
+  // check for duplicate task name
+  if (document.getElementById(taskValue)) {
+    alert("Task with this name already exists");
+    return;
+  }
+
   //create new div using value for ids
   var newItem = `<div id="${taskValue}">
-                    <p contenteditable="true">${taskValue}</p><br>
+                    <p>${taskValue}</p><br>
+                    <button id="${taskValue}edit" onclick="onClickEditButton(this)">Edit Task</button>
                     <button id="${taskValue}button" onclick="onClickDeleteButton(this)">Delete Task</button>
                   </div>`;
 
@@ -23,11 +36,62 @@ function onClickCreateTask() {
 
   // add new div
   activeTasks.innerHTML += newItem;
+
+  // clears input box
+  createTask.value = "";
+
 }
 
-// Delete parent div based on element
+// delete parent div based on element
 function onClickDeleteButton(e) {
 
   const elementToRemove = e.parentNode;
   elementToRemove.parentNode.removeChild(elementToRemove);
+}
+
+// edit task text
+function onClickEditButton(e) {
+
+  // get the paragraph within task div
+  const taskDiv = e.parentNode;
+  const paragraph = taskDiv.querySelector("p");
+
+  // edit task name
+  if (paragraph.isContentEditable) {
+
+    // turns off editbale text
+    paragraph.contentEditable = "false";
+    // changes button text
+    e.innerText = "Edit Task";
+
+    // create new task value
+    const newTaskValue = paragraph.innerText.trim();
+
+    // checks for empty task name
+    if (newTaskValue === "") {
+      alert("Task name cannot be empty.");
+      // keeps old name
+      paragraph.innerText = taskDiv.id;
+      return;
+    }
+
+    // checks for duplicate task name
+    if (document.getElementById(newTaskValue) && newTaskValue !== taskDiv.id) {
+      alert("A task with this name already exists.");
+      // keeps old name
+      paragraph.innerText = taskDiv.id;
+      return;
+    }
+
+  } else {
+
+    // turns on editbale text
+    paragraph.contentEditable = "true";
+    // highlights box
+    paragraph.focus();
+    // changes button text
+    e.innerText = "Save Task";
+
+  }
+  
 }
