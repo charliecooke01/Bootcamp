c31827e5af2f61c8b06877a25a6c0f32b9fb7c21
charliecooke01 <charlielcooke@googlemail.com>
2025-07-29 21:54:59 +0100

    Week 9 S2

--- MyWork/week-9/W9-S2-Style-and-State/code/src/components/BusinessCard.jsx
+++ MyWork/week-9/W9-S2-Style-and-State/code/src/components/BusinessCard.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './BusinessCard.css';
+import '../index.css';
+import myImage from '/Bootcamp/MyWork/week-9/W9-S2-Style-and-State/5_Images/exercise/images/40.jpg';
 
 function BusinessCard({contact}) {
 
-  const {name, position, email, phone, website} = contact;
+  const {name, position, email, phone, website, photo} = contact;
+
   return (
     <div className="business-card">
       <div className="photo-placeholder">
-        {/* Placeholder for image */}
+        <img className="avatar-image" src={myImage} alt="My Image" />
       </div>
       <div className="contact-info">
         <h2>{name}</h2>
